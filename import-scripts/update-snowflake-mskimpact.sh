587e6bcab8e7659316ee579fafc59a724bd3a89a
Calla Chennault <51501715+callachennault@users.noreply.github.com>
2025-10-16 12:45:45 -0400

    script for snowflake chord data

---
+++ import-scripts/update-snowflake-mskimpact.sh
@@ -0,0 +1,461 @@
+#!/usr/bin/env bash
+
+IMPORT_SCRIPTS_DIR=$1
+CDM_METADATA=$2
+
+export CURRENT_DATE="$(date '+%m.%d.%y')"
+export CDD_URL="https://cdd.cbioportal.mskcc.org/api/"
+
+# TODO make an input arg?
+export OUTPUT_DIR=""
+export OUTPUT_TMPDIR="$OUTPUT_DIR/tmp"
+
+# Patient and sample attributes that we want to deliver in our data
+DELIVERED_PATIENT_ATTRIBUTES="PATIENT_ID PARTC_CONSENTED_12_245 RACE SEX ETHNICITY CURRENT_AGE_DEID STAGE_HIGHEST_RECORDED NUM_ICDO_DX ADRENAL_GLANDS BONE CNS_BRAIN INTRA_ABDOMINAL LIVER LUNG LYMPH_NODES OTHER PLEURA REPRODUCTIVE_ORGANS SMOKING_PREDICTIONS_3_CLASSES GLEASON_FIRST_REPORTED GLEASON_HIGHEST_REPORTED HISTORY_OF_PDL1 HISTORY_OF_D_MMR PRIOR_MED_TO_MSK OS_MONTHS OS_STATUS"
+DELIVERED_SAMPLE_ATTRIBUTES="SAMPLE_ID PATIENT_ID CANCER_TYPE SAMPLE_TYPE SAMPLE_CLASS METASTATIC_SITE PRIMARY_SITE CANCER_TYPE_DETAILED GENE_PANEL SAMPLE_COVERAGE TUMOR_PURITY ONCOTREE_CODE MSI_SCORE MSI_TYPE SOMATIC_STATUS ARCHER CVR_TMB_COHORT_PERCENTILE CVR_TMB_SCORE CVR_TMB_TT_COHORT_PERCENTILE SEQ_DATE"
+
+# Duplicate columns that we want to filter out of MAF files
+MUTATIONS_EXTENDED_COLS_TO_FILTER="amino_acid_change,cdna_change,transcript,COMMENTS,Comments,comments,Matched_Norm_Sample_Barcode"
+NONSIGNEDOUT_COLS_TO_FILTER="amino_acid_change,cdna_change,transcript,Comments,comments,Matched_Norm_Sample_Barcode"
+
+# Stores an array of clinical attributes found in the data + attributes we want to filter, respectively
+unset clinical_attributes_in_file
+declare -gA clinical_attributes_in_file=() 
+unset clinical_attributes_to_filter_arg
+declare -g clinical_attributes_to_filter_arg="unset"
+
+source $IMPORT_SCRIPTS_DIR/dmp-import-vars-functions.sh
+source $IMPORT_SCRIPTS_DIR/filter-clinical-arg-functions.sh
+
+function report_error() {
+    # Error message provided as an argument
+    error_message="$1"
+    echo -e "ERROR: $error_message. Exiting."
+    exit 1
+}
+
+function pull_latest_data_from_git_repo() {
+    (   # Executed in a subshell to avoid changing the actual working directory
+        # If any statement fails, the return value of the entire expression is the failure status
+        cd $OUTPUT_DIR &&
+        $GIT_BINARY fetch &&
+        $GIT_BINARY reset origin/main --hard &&
+        $GIT_BINARY lfs pull &&
+        $GIT_BINARY clean -f -d
+    )
+}
+
+function setup_data_directories() {
+    # Create temporary directory to store data before subsetting
+    if ! [ -d "$OUTPUT_TMPDIR" ] ; then
+        if ! mkdir -p "$OUTPUT_TMPDIR" ; then
+            echo "Failed to create temporary directory"
+            return 1
+        fi
+    fi
+
+    # If tmp dir exists and is not empty, remove the contents
+    if [[ -d "$OUTPUT_TMPDIR" && "$OUTPUT_TMPDIR" != "/" ]] ; then
+        rm -rf "$OUTPUT_TMPDIR"/*
+        if [ $? -gt 0 ] ; then
+            echo "Failed to remove contents of temporary directory"
+            return 1
+        fi
+    fi
+
+    # Copy MSKSOLIDHEME dataset to the temporary directory
+    cp -a $MSK_SOLID_HEME_DATA_HOME/* $OUTPUT_TMPDIR
+    if [ $? -gt 0 ] ; then
+        echo "Failed to populate temporary directory"
+        return 1
+    fi
+
+}
+
+function generate_subset() {
+    # Generate subset of Part A consented patients from MSK-Impact
+    $PYTHON_BINARY $IMPORT_SCRIPTS_DIR/generate-clinical-subset.py \
+        --study-id="mskimpact" \
+        --clinical-file="$OUTPUT_TMPDIR/data_clinical_patient.txt" \
+        --filter-criteria="PARTA_CONSENTED_12_245=YES" \
+        --subset-filename="$OUTPUT_TMPDIR/part_a_subset.txt"
+    if [ $? -gt 0 ] ; then
+        echo "Failed to generate list of consented patients"
+        return 1
+    fi
+
+    # Write out the subsetted data
+    $PYTHON_BINARY $IMPORT_SCRIPTS_DIR/merge.py \
+        --study-id="mskimpact" \
+        --subset="$OUTPUT_TMPDIR/part_a_subset.txt" \
+        --output-directory="$OUTPUT_DIR" \
+        --merge-clinical="true" \
+        $OUTPUT_TMPDIR
+    if [ $? -gt 0 ] ; then
+        echo "Failed to subset on list of consented patients"
+        return 1
+    fi
+
+    # Add CDM timeline files
+    sh $IMPORT_SCRIPTS_DIR/subset-cdm-timeline-files.sh "mskimpact" $OUTPUT_DIR $OUTPUT_TMPDIR
+    if [ $? -gt 0 ] ; then
+        echo "Failed to generate CDM timeline files"
+        return 1
+    fi
+}
+
+function filter_clinical_cols() {
+    # Filter columns from patient file
+    PATIENT_INPUT_FILEPATH="$OUTPUT_DIR/data_clinical_patient.txt"
+    if ! filter_clinical_attribute_columns "$PATIENT_INPUT_FILEPATH" "$DELIVERED_PATIENT_ATTRIBUTES" ; then
+        echo "Failed to filter clinical patient attributes"
+        return 1
+    fi
+
+    # Filter columns from sample file
+    SAMPLE_INPUT_FILEPATH="$OUTPUT_DIR/data_clinical_sample.txt"
+    if ! filter_clinical_attribute_columns "$SAMPLE_INPUT_FILEPATH" "$DELIVERED_SAMPLE_ATTRIBUTES" ; then
+        echo "Failed to filter clinical sample attributes"
+        return 1
+    fi
+}
+
+function standardize_clinical_data() {
+    PATIENT_INPUT_FILEPATH="$OUTPUT_DIR/data_clinical_patient.txt"
+    PATIENT_OUTPUT_FILEPATH="$OUTPUT_DIR/data_clinical_patient.txt.standardized"
+    SAMPLE_INPUT_FILEPATH="$OUTPUT_DIR/data_clinical_sample.txt"
+    SAMPLE_OUTPUT_FILEPATH="$OUTPUT_DIR/data_clinical_sample.txt.standardized"
+
+    # Standardize the clinical files to use NA for blank values
+    $PYTHON_BINARY $IMPORT_SCRIPTS_DIR/standardize_clinical_data.py -f "$PATIENT_INPUT_FILEPATH" > "$PATIENT_OUTPUT_FILEPATH" &&
+    $PYTHON_BINARY $IMPORT_SCRIPTS_DIR/standardize_clinical_data.py -f "$SAMPLE_INPUT_FILEPATH" > "$SAMPLE_OUTPUT_FILEPATH" &&
+
+    # Rewrite the patient and sample files with updated data
+    mv "$PATIENT_OUTPUT_FILEPATH" "$PATIENT_INPUT_FILEPATH" &&
+    mv "$SAMPLE_OUTPUT_FILEPATH" "$SAMPLE_INPUT_FILEPATH"
+}
+
+# Skip for snowflake
+function add_seq_date_to_sample_file() {
+    DATABRICKS_SERVER_HOSTNAME=`grep server_hostname $DATABRICKS_CREDS_FILE | sed 's/^.*=//g'`
+    DATABRICKS_HTTP_PATH=`grep http_path $DATABRICKS_CREDS_FILE | sed 's/^.*=//g'`
+    DATABRICKS_TOKEN=`grep access_token $DATABRICKS_CREDS_FILE | sed 's/^.*=//g'`
+    SEQ_DATE_FILEPATH="$CDSI_DATA_HOME/seq_date.txt"
+
+    SAMPLE_INPUT_FILEPATH="$OUTPUT_DIR/data_clinical_sample.txt"
+    SAMPLE_OUTPUT_FILEPATH="$OUTPUT_DIR/data_clinical_sample.txt.with_seq_date"
+    KEY_COLUMNS="SAMPLE_ID PATIENT_ID"
+
+    # Download seq_date.txt file from DataBricks
+    $PYTHON3_BINARY $IMPORT_SCRIPTS_DIR/databricks_query_py3.py \
+        --server-hostname $DATABRICKS_SERVER_HOSTNAME \
+        --http-path $DATABRICKS_HTTP_PATH \
+        --access-token $DATABRICKS_TOKEN \
+        query \
+        --sql-query "SELECT DMP_SAMPLE_ID AS SAMPLE_ID, DMP_PATIENT_ID AS PATIENT_ID, SEQUENCING_DATE AS SEQ_DATE FROM cdsi_eng_phi.msk_impact_dates.sequencing_date" \
+        --output-path $SEQ_DATE_FILEPATH
+    if [ $? -gt 0 ] ; then
+        echo "Failed to query DataBricks for SEQ_DATE file"
+        return 1
+    fi
+
+    # Add SEQ_DATE column to sample file and overwrite the previous sample file
+    $PYTHON3_BINARY $IMPORT_SCRIPTS_DIR/combine_files_py3.py -i "$SAMPLE_INPUT_FILEPATH" "$SEQ_DATE_FILEPATH" -o "$SAMPLE_OUTPUT_FILEPATH" -c $KEY_COLUMNS -m left &&
+    mv "$SAMPLE_OUTPUT_FILEPATH" "$SAMPLE_INPUT_FILEPATH"
+    if [ $? -gt 0 ] ; then
+        echo "Failed to merge $SAMPLE_INPUT_FILEPATH and $SEQ_DATE_FILEPATH"
+        return 1
+    fi
+}
+
+function add_metadata_headers() {
+    # Calling merge.py strips out metadata headers from our clinical files - add them back in
+    INPUT_FILENAMES="$OUTPUT_DIR/data_clinical_sample.txt $OUTPUT_DIR/data_clinical_patient.txt"
+    $PYTHON_BINARY $IMPORT_SCRIPTS_DIR/add_clinical_attribute_metadata_headers.py -f $INPUT_FILENAMES -c "$CDD_URL" -s mskimpact -i $CDM_METADATA
+}
+
+function standardize_cna_data() {
+    # Standardize the CNA file to use NA for blank values
+    DATA_CNA_INPUT_FILEPATH="$OUTPUT_DIR/data_CNA.txt"
+    $PYTHON_BINARY $IMPORT_SCRIPTS_DIR/standardize_cna_data.py -f "$DATA_CNA_INPUT_FILEPATH"
+}
+
+# Skip for snowflake
+function transpose_cna_data() {
+    # Transpose the CNA file so that sample IDs are contained in the first column instead of the header
+    DATA_CNA_INPUT_FILEPATH="$OUTPUT_DIR/data_CNA.txt"
+    $PYTHON3_BINARY $IMPORT_SCRIPTS_DIR/transpose_cna_py3.py "$DATA_CNA_INPUT_FILEPATH"
+}
+
+function standardize_mutations_data() {
+    MUTATIONS_EXTD_INPUT_FILEPATH="$OUTPUT_DIR/data_mutations_extended.txt"
+    NSOUT_MUTATIONS_INPUT_FILEPATH="$OUTPUT_DIR/data_mutations_non_signedout.txt"
+
+    # Standardize the mutations files to check for valid values in the 'NCBI_Build' column
+    $PYTHON_BINARY $IMPORT_SCRIPTS_DIR/standardize_mutations_data.py -f "$MUTATIONS_EXTD_INPUT_FILEPATH" &&
+    $PYTHON_BINARY $IMPORT_SCRIPTS_DIR/standardize_mutations_data.py -f "$NSOUT_MUTATIONS_INPUT_FILEPATH"
+}
+
+function remove_duplicate_maf_variants() {
+    MUTATIONS_EXTD_INPUT_FILEPATH="$OUTPUT_DIR/data_mutations_extended.txt"
+    NSOUT_MUTATIONS_INPUT_FILEPATH="$OUTPUT_DIR/data_mutations_non_signedout.txt"
+
+    MUTATIONS_EXTD_OUTPUT_FILEPATH="$OUTPUT_DIR/data_mutations_extended_merged.txt"
+    NSOUT_MUTATIONS_OUTPUT_FILEPATH="$OUTPUT_DIR/data_mutations_non_signedout_merged.txt"
+
+    # Remove duplicate variants from MAF files
+    # CVR data can contain duplicates for a gene and its alias
+    $PYTHON_BINARY $IMPORT_SCRIPTS_DIR/remove-duplicate-maf-variants.py -i "$MUTATIONS_EXTD_INPUT_FILEPATH" -o "$MUTATIONS_EXTD_OUTPUT_FILEPATH" &&
+    $PYTHON_BINARY $IMPORT_SCRIPTS_DIR/remove-duplicate-maf-variants.py -i "$NSOUT_MUTATIONS_INPUT_FILEPATH" -o "$NSOUT_MUTATIONS_OUTPUT_FILEPATH" &&
+
+    # Rewrite mutation files with updated data
+    mv "$MUTATIONS_EXTD_OUTPUT_FILEPATH" "$MUTATIONS_EXTD_INPUT_FILEPATH" &&
+    mv "$NSOUT_MUTATIONS_OUTPUT_FILEPATH" "$NSOUT_MUTATIONS_INPUT_FILEPATH"
+}
+
+function filter_germline_events_from_maf() {
+    MUTATION_FILEPATH="$OUTPUT_DIR/data_mutations_extended.txt"
+    MUTATION_FILTERED_FILEPATH="$OUTPUT_DIR/data_mutations_extended.txt.filtered"
+    $PYTHON3_BINARY $IMPORT_SCRIPTS_DIR/filter_non_somatic_events_py3.py $MUTATION_FILEPATH $MUTATION_FILTERED_FILEPATH --event-type mutation
+    if [ $? -gt 0 ] ; then
+        echo "Failed to filter germline events from mutation file"
+        return 1
+    fi
+    mv $MUTATION_FILTERED_FILEPATH $MUTATION_FILEPATH
+
+    MUTATION_FILEPATH="$OUTPUT_DIR/data_mutations_non_signedout.txt"
+    MUTATION_FILTERED_FILEPATH="$OUTPUT_DIR/data_mutations_non_signedout.txt.filtered"
+    $PYTHON3_BINARY $IMPORT_SCRIPTS_DIR/filter_non_somatic_events_py3.py $MUTATION_FILEPATH $MUTATION_FILTERED_FILEPATH --event-type mutation
+    if [ $? -gt 0 ] ; then
+        echo "Failed to filter germline events from nonsignedout mutation file"
+        return 1
+    fi
+    mv $MUTATION_FILTERED_FILEPATH $MUTATION_FILEPATH
+}
+
+# TODO We do this for sophia but not for AZ - what about snowflake?
+function remove_sequenced_samples_header() {
+    MAF_INPUT_FILEPATH="$OUTPUT_DIR/data_mutations_extended.txt"
+    MAF_OUTPUT_FILEPATH="$OUTPUT_DIR/data_mutations_extended.txt.tmp"
+
+    # This removes the sequenced_samples header from the MAF
+    awk '!/^#sequenced_samples:/' "$MAF_INPUT_FILEPATH" > "$MAF_OUTPUT_FILEPATH" && mv "$MAF_OUTPUT_FILEPATH" "$MAF_INPUT_FILEPATH"
+}
+
+function filter_replicated_maf_columns() {
+    MUTATIONS_EXTENDED_INPUT_FILEPATH="$OUTPUT_DIR/data_mutations_extended.txt"
+    MUTATIONS_EXTENDED_OUTPUT_FILEPATH="$OUTPUT_DIR/data_mutations_extended.txt.filtered"
+    MUTATIONS_NONSIGNEDOUT_INPUT_FILEPATH="$OUTPUT_DIR/data_mutations_non_signedout.txt"
+    MUTATIONS_NONSIGNEDOUT_OUTPUT_FILEPATH="$OUTPUT_DIR/data_mutations_non_signedout.txt.filtered"
+
+    # Filter out the columns we want to exclude in both files
+    $PYTHON_BINARY $IMPORT_SCRIPTS_DIR/filter_clinical_data.py -c "$MUTATIONS_EXTENDED_INPUT_FILEPATH" -e "$MUTATIONS_EXTENDED_COLS_TO_FILTER" > "$MUTATIONS_EXTENDED_OUTPUT_FILEPATH" &&
+    $PYTHON_BINARY $IMPORT_SCRIPTS_DIR/filter_clinical_data.py -c "$MUTATIONS_NONSIGNEDOUT_INPUT_FILEPATH" -e "$NONSIGNEDOUT_COLS_TO_FILTER" > "$MUTATIONS_NONSIGNEDOUT_OUTPUT_FILEPATH" &&
+
+    # Rewrite the MAF files
+    mv "$MUTATIONS_EXTENDED_OUTPUT_FILEPATH" "$MUTATIONS_EXTENDED_INPUT_FILEPATH" &&
+    mv "$MUTATIONS_NONSIGNEDOUT_OUTPUT_FILEPATH" "$MUTATIONS_NONSIGNEDOUT_INPUT_FILEPATH"
+}
+
+function standardize_structural_variant_data() {
+    DATA_SV_INPUT_FILEPATH="$OUTPUT_DIR/data_sv.txt"
+    $PYTHON_BINARY $IMPORT_SCRIPTS_DIR/standardize_structural_variant_data.py -f "$DATA_SV_INPUT_FILEPATH"
+}
+
+function filter_germline_events_from_sv() {
+    SV_FILEPATH="$OUTPUT_DIR/data_sv.txt"
+    SV_FILTERED_FILEPATH="$OUTPUT_DIR/data_sv.txt.filtered"
+    $PYTHON3_BINARY $IMPORT_SCRIPTS_DIR/filter_non_somatic_events_py3.py $SV_FILEPATH $SV_FILTERED_FILEPATH --event-type structural_variant
+    if [ $? -gt 0 ] ; then
+        echo "Failed to filter germline events from structural variant file"
+        return 1
+    fi
+    mv $SV_FILTERED_FILEPATH $SV_FILEPATH
+}
+
+function filter_files_in_delivery_directory() {
+    unset filenames_to_deliver
+    declare -A filenames_to_deliver
+
+    # Data files to deliver
+    filenames_to_deliver[data_clinical_patient.txt]+=1
+    filenames_to_deliver[data_clinical_sample.txt]+=1
+    filenames_to_deliver[data_CNA.txt]+=1
+    filenames_to_deliver[data_gene_matrix.txt]+=1
+    filenames_to_deliver[data_mutations_extended.txt]+=1
+    filenames_to_deliver[data_mutations_non_signedout.txt]+=1
+    filenames_to_deliver[data_sv.txt]+=1
+    filenames_to_deliver[mskimpact_data_cna_hg19.seg]+=1
+    filenames_to_deliver[data_timeline_bmi.txt]+=1
+    filenames_to_deliver[data_timeline_ca_125_labs.txt]+=1
+    filenames_to_deliver[data_timeline_ca_15-3_labs.txt]+=1
+    filenames_to_deliver[data_timeline_ca_19-9_labs.txt]+=1
+    filenames_to_deliver[data_timeline_cancer_presence.txt]+=1
+    filenames_to_deliver[data_timeline_cea_labs.txt]+=1
+    filenames_to_deliver[data_timeline_diagnosis.txt]+=1
+    filenames_to_deliver[data_timeline_ecog_kps.txt]+=1
+    filenames_to_deliver[data_timeline_follow_up.txt]+=1
+    filenames_to_deliver[data_timeline_gleason.txt]+=1
+    filenames_to_deliver[data_timeline_mmr.txt]+=1
+    filenames_to_deliver[data_timeline_pdl1.txt]+=1
+    filenames_to_deliver[data_timeline_prior_meds.txt]+=1
+    filenames_to_deliver[data_timeline_progression.txt]+=1
+    filenames_to_deliver[data_timeline_psa_labs.txt]+=1
+    filenames_to_deliver[data_timeline_radiation.txt]+=1
+    filenames_to_deliver[data_timeline_specimen_surgery.txt]+=1
+    filenames_to_deliver[data_timeline_specimen.txt]+=1
+    filenames_to_deliver[data_timeline_surgery.txt]+=1
+    filenames_to_deliver[data_timeline_treatment.txt]+=1
+    filenames_to_deliver[data_timeline_tsh_labs.txt]+=1
+    filenames_to_deliver[data_timeline_tumor_sites.txt]+=1
+
+    # Remove any files/directories that are not specified above
+    for filepath in $OUTPUT_DIR/* ; do
+        filename=$(basename $filepath)
+        if [ -z ${filenames_to_deliver[$filename]} ] ; then
+            if ! rm -rf $filepath ; then
+                return 1
+            fi
+        fi
+    done
+    return 0
+}
+
+function push_updates_to_git_repo() {
+    (   # Executed in a subshell to avoid changing the actual working directory
+        # If any statement fails, the return value of the entire expression is the failure status
+        cd $OUTPUT_DIR &&
+        $GIT_BINARY add * &&
+        $GIT_BINARY commit -m "chord data $CURRENT_DATE" &&
+        $GIT_BINARY push origin
+    )
+}
+
+function zip_files_for_delivery() {
+    # Zip data files for easier data transfer
+    zip "$OUTPUT_DIR/chord-data-$CURRENT_DATE.zip" $OUTPUT_DIR/*data*
+}
+
+function cleanup_repo() {
+    # Remove temporary directory now that the subset has been merged and post-processed
+    if [[ -d "$OUTPUT_TMPDIR" && "$OUTPUT_TMPDIR" != "/" ]] ; then
+        rm -rf "$OUTPUT_TMPDIR"
+    fi
+
+    # Clean up untracked files and LFS objects
+    bash $IMPORT_SCRIPTS_DIR/datasource-repo-cleanup.sh $OUTPUT_DIR
+}
+
+printTimeStampedDataProcessingStepMessage "Subset MSK-IMPACT"
+
+# Pull latest from data repo
+#if ! pull_latest_data_from_git_repo ; then
+#    report_error "ERROR: Failed git pull"
+#fi
+
+# Create temporary directories
+if ! setup_data_directories ; then 
+    report_error "Failed to set up data directories"
+fi
+
+# Generate Part A consented subset of MSKSOLIDHEME
+if ! generate_subset ; then
+    report_error "Failed to generate data"
+fi
+
+printTimeStampedDataProcessingStepMessage "Post-process clinical files"
+
+# Filter clinical attribute columns from clinical files
+if ! filter_clinical_cols ; then
+    report_error "Failed to filter non-delivered clinical attribute columns"
+fi
+
+# Standardize blank clinical data values to NA
+if ! standardize_clinical_data ; then
+    report_error "ERROR: Failed to standardize blank clinical data values to NA"
+fi
+
+# Add SEQ_DATE to clinical sample file
+#if ! add_seq_date_to_sample_file; then
+#    report_error "ERROR: Failed to add SEQ_DATE column to clinical sample file"
+#fi
+
+# Add metadata headers to clinical files
+if ! add_metadata_headers ; then
+    report_error "Failed to add metadata headers to clinical attribute files"
+fi
+
+printTimeStampedDataProcessingStepMessage "Post-process CNA file"
+
+# Standardize blank CNA data values to NA
+if ! standardize_cna_data ; then
+    report_error "Failed to standardize blank CNA data values to NA"
+fi
+
+# Transpose CNA file
+#if ! transpose_cna_data ; then
+#    report_error "Failed to transpose CNA file"
+#fi
+
+printTimeStampedDataProcessingStepMessage "Post-process MAF files"
+
+# Standardize mutations files
+if ! standardize_mutations_data ; then
+    report_error "Failed to standardize mutations files"
+fi
+
+# Remove duplicate variants from MAF files
+if ! remove_duplicate_maf_variants ; then
+    report_error "Failed to remove duplicate variants from MAF files"
+fi
+
+# Filter germline events from mutation file
+if ! filter_germline_events_from_maf ; then
+    report_error "Failed to filter MAF germline events"
+fi
+
+# Remove sequenced_samples header from MAF
+if ! remove_sequenced_samples_header ; then
+    report_error "Failed to remove sequenced_samples header from MAF"
+fi
+
+# Filter replicated columns from MAF files
+if ! filter_replicated_maf_columns ; then
+    report_error "Failed to filter duplicated columns in MAF files"
+fi
+
+printTimeStampedDataProcessingStepMessage "Post-process SV file"
+
+# Standardize structural variant data by removing records with invalid genes and standardizing the file header
+if ! standardize_structural_variant_data ; then
+    report_error "Failed to standardize structural variant data"
+fi
+
+# Filter germline events from mutation file and structural variant file
+if ! filter_germline_events_from_sv ; then
+    report_error "Failed to filter SV germline events"
+fi
+
+printTimeStampedDataProcessingStepMessage "Finalize and validate study contents"
+
+# Filter out files which are not delivered
+if ! filter_files_in_delivery_directory ; then
+    report_error "Failed to filter non-delivered files"
+fi
+
+printTimeStampedDataProcessingStepMessage "Push data to GitHub"
+
+# Push updated data to GitHub
+if ! push_updates_to_git_repo ; then
+    report_error "Failed git push"
+fi
+
+# Compress files for easier download
+if ! zip_files_for_delivery ; then
+    report_error "Failed to compress study files"
+fi
+
+# Cleanup git repo
+if ! cleanup_repo ; then
+    report_error "Failed to cleanup git repository"
+fi
